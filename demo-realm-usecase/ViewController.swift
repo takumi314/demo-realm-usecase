6f3117f8f5a1a65874b5696a74d98e50f9e7b80d
takumi314 <isospin_symmetry@me.com>
2018-08-05 16:17:59 +0900

    Initial commit

---
+++ demo-realm-usecase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo-realm-usecase
+//
+//  Created by NishiokaKohei on 2018/08/05.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
