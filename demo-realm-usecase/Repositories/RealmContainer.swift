59d04223f34260d2b5ba6c089b3cab1ac0d33095
takumi314 <isospin_symmetry@me.com>
2018-08-06 07:20:51 +0900

    [WIP] make FetchedResults

--- demo-realm-usecase/Repositories/RealmContainer.swift
+++ demo-realm-usecase/Repositories/RealmContainer.swift
@@ -41,27 +41,3 @@ struct RealmContainer {
     }
 
 }
-
-
-/// データ取得用のラッパークラス
-final class FetchedResults<T: Persistable> {
-
-    private let results: Results<T.ManagedObject>
-
-    public var count: Int {
-        return results.count
-    }
-
-    init(results: Results<T.ManagedObject>) {
-        self.results = results
-    }
-
-//    public func value(at index: Int) -> T {
-//        return T(results[index])
-//    }
-
-//    public func valus() -> [T] {
-//        return results.sorted(byKeyPath: "name", ascending: true).map({ T($0) })
-//    }
-
-}
